67641f6926bd8aeb89b418f0fdd1a4a574e5cb81
AzraelChernoff <155092697+AzraelChernoff@users.noreply.github.com>
2025-04-23 18:32:19 -0300

    cpmtooldev.py

--- cpmtooldev.py
+++ cpmtooldev.py
@@ -4,7 +4,7 @@ import requests
 import subprocess
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://cpmazraeldevs.squareweb.app/"
+__ENDPOINT_URL__: str = "https://cpmazraeldevs.squareweb.app/api/"
 
 class CPMTooldev:
     def __init__(self, access_key) -> None:
